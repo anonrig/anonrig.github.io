447b34ba64209efe4cdd9e28dd34db96542eb0de
Yagiz Nizipli <yagiznizipli@me.com>
2016-05-30 21:56:08 +0300

    Add conferences

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 (function($){
   $(function(){
+    var link = 'https://www.youtube.com/embed/0qbEZ8PBNm0';
+
     $('.parallax').parallax();
     $('.slider').slider({full_width: true});
+
+    $('.conference-button').click(function(event) {
+      var attr = this.attributes['data-url'].nodeValue;
+
+      if (link == attr)
+        return;
+
+      link = attr;
+
+      $('#video-el')[0].src = link;
+    })
   });
 })(jQuery);
